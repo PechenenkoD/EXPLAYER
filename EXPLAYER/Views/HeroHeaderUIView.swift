76c6eb1298029c3d0463ea1d48bcbb6c2dd00441
d.pechenenko <pechenenkod@gmail.com>
2023-12-12 11:03:59 +0100

    add HeroHeaderUIView

---
+++ EXPLAYER/Views/HeroHeaderUIView.swift
@@ -0,0 +1,37 @@
+//
+//  HeroHeaderUIView.swift
+//  EXPLAYER
+//
+//  Created by Dmitro Pechenenko on 12.12.2023.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+
+}
