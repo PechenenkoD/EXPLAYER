184da66fca1cb36eeebbbf697fed20c0c8db1597
d.pechenenko <pechenenkod@gmail.com>
2023-12-15 13:14:16 +0100

    work in progress

---
+++ EXPLAYER/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  EXPLAYER
+//
+//  Created by Dmitro Pechenenko on 15.12.2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
