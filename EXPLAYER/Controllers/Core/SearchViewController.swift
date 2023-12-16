5673d8e4a589cc28bd524b01ea044e589a6edb55
d.pechenenko <pechenenkod@gmail.com>
2023-12-16 19:35:39 +0100

    work in progress

--- EXPLAYER/Controllers/Core/SearchViewController.swift
+++ EXPLAYER/Controllers/Core/SearchViewController.swift
@@ -17,6 +17,12 @@ class SearchViewController: UIViewController {
         return table
     }()
     
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for a movie or a tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +36,8 @@ class SearchViewController: UIViewController {
         
         discoverTable.delegate = self
         discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
         
         fetchDiscoverMovies()
     }
---
+++ EXPLAYER/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  EXPLAYER
+//
+//  Created by Dmitro Pechenenko on 15.12.2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
+    }
+
+}
